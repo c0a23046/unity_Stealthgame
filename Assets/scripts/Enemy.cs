05b60f7c41929254f4e9473c9ff47b005709abcd
shinnosuke <syunkun1973@gmail.com>
2025-10-17 00:32:22 +0900

    Update scripts: 敵スクリプトの作成

--- Assets/scripts/Enemy.cs
+++ Assets/scripts/Enemy.cs
@@ -4,15 +4,32 @@ using UnityEngine;
 
 public class Enemy : MonoBehaviour
 {
+    //public GameObject enemy;  
+    public float moveSpeed; //移動速度
+    public float moveTime; //移動する時間
+    public float waitTime;
+    public Animator EnemyAnimator;
+
+    bool isWalk; //歩くアニメーション
+
+
     // Start is called before the first frame update
     void Start()
     {
-        
+        isWalk = false;
     }
 
     // Update is called once per frame
     void Update()
     {
-        
+        Ptorol();
     }
+    
+    void Ptorol()
+    {
+        isWalk = true;
+        transform.Translate(0f, 0f, moveSpeed);
+        EnemyAnimator.SetBool("walk", isWalk);
+    }
+
 }
